fc3049923730d7d004f0b54bfd9ffdcac4984aef
Flemming N Larsen <flemming.n.larsen@gmail.com>
2006-06-26 20:34:13 +0000

    Changed to use true HSL color model in order to correct bug where e.g. the radar was not coloured correctly

--- robocode/robocode/util/ImageUtil.java
+++ robocode/robocode/util/ImageUtil.java
@@ -101,10 +101,10 @@ public class ImageUtil {
 	 * @author Flemming N. Larsen
 	 */
 	private static class ColorFilter extends RGBImageFilter {
-		private float[] _hsb;
+		private float[] hsl;
 
 		public ColorFilter(Color color) {
-			_hsb = Color.RGBtoHSB(color.getRed(), color.getGreen(), color.getBlue(), null);
+			hsl = RGBtoHSL(color.getRed(), color.getGreen(), color.getBlue());
 		}
 
 		public int filterRGB(int x, int y, int argb) {
@@ -112,58 +112,86 @@ public class ImageUtil {
 			int g = (argb >> 8) & 0xff;
 			int b = argb & 0xff;
 
-			float[] hsb = Color.RGBtoHSB(r, g, b, null);
+			float[] HSL = RGBtoHSL(r, g, b);
 
-			if (hsb[1] > 0f) {
-				if (_hsb[1] > 0f) {
-					return (argb & 0xff000000) | HSLtoRGB(_hsb[0], _hsb[1], hsb[2]);
-				}
-				return (argb & 0xff000000) | HSLtoRGB(0f, 0f, (hsb[2] + _hsb[2]) / 2);
+			if (HSL[1] > 0) {
+				return argb & 0xff000000 | HSLtoRGB(hsl[0], hsl[1], (HSL[2] + hsl[2]) / 2);
 			}
 			return argb;
 		}
 	}
 
-	/**
-	 * Return RGB equivalent of HSL value. In the HSL color model, 0 hue (red) and 100% luminance means White; to
-	 * <tt>Color.HSBtoRGB()</tt>, 0 hue and 100% luminance means Red.
-	 *
-	 * @param h hue, in the range 0..1
-	 * @param s saturation, in the range 0..1
-	 * @param l luminance, in the range 0..1
-	 * @return rgb equivalent of the hsl values
-	 *
-	 * @see Color#HSBtoRGB()
-	 */
-	private static final int HSLtoRGB(float h, float s, float l) {
-		float m2 = (l <= 0.5) ? (l * (s + 1f)) : (l + s - l * s);
-		float m1 = 2f * l - m2;
+	private static float[] RGBtoHSL(int r, int g, int b) {
+		float R = (float) r / 255;
+		float G = (float) g / 255;
+		float B = (float) b / 255;
+		
+		float min = Math.min(Math.min(R, G), B); // Min. value of RGB
+		float max = Math.max(Math.max(R, G), B); // Max. value of RGB
+		float delta = max - min; // Delta RGB value
 
-		int r = (int) (255 * HUEtoRGB(m1, m2, h + (1f / 3f)));
+		float L = (max + min) / 2;
+
+		float H, S;
+		
+		if (delta == 0) { // This is a gray, no chroma...
+			H = 0;
+			S = 0;
+		} else { // Chromatic data...
+			if (L < 0.5f) {
+				S = delta / (max + min);
+			} else {
+				S = delta / (2 - max - min);
+			}
+
+			float deltaR = (((max - R) / 6) + (delta / 2)) / delta;
+			float deltaG = (((max - G) / 6) + (delta / 2)) / delta;
+			float deltaB = (((max - B) / 6) + (delta / 2)) / delta;
+
+			if (R == max) {
+				H = deltaB - deltaG;
+			} else if (G == max) {
+				H = (1f / 3) + deltaR - deltaB;
+			} else {
+				H = (2f / 3) + deltaG - deltaR;
+			}
+
+			if (H < 0) {
+				H++;
+			}
+			if (H > 1) {
+				H--;
+			}
+		}
+		return new float[] { H, S, L };
+	}
+	
+	private static int HSLtoRGB(float h, float s, float l) {
+		float m2 = (l <= 0.5f) ? (l * (s + 1)) : (l + s - l * s);
+		float m1 = 2 * l - m2;
+
+		int r = (int) (255 * HUEtoRGB(m1, m2, h + (1f / 3)));
 		int g = (int) (255 * HUEtoRGB(m1, m2, h));
-		int b = (int) (255 * HUEtoRGB(m1, m2, h - (1f / 3f)));
+		int b = (int) (255 * HUEtoRGB(m1, m2, h - (1f / 3)));
 
 		return (((r << 8) | g) << 8) | b;
 	}
 
-	/**
-	 * Copied from <a href=http://www.w3.org/TR/css3-color/>http://www.w3.org/TR/css3-color/</a>.
-	 */
 	private static float HUEtoRGB(float m1, float m2, float h) {
-		if (h < 0f) {
-			h += 1f;
+		if (h < 0) {
+			h++;
 		}
-		if (h > 1f) {
-			h -= 1f;
+		if (h > 1) {
+			h--;
 		}
-		if ((h * 6f) < 1f) {
-			return m1 + (m2 - m1) * h * 6f;
+		if ((h * 6) < 1) {
+			return m1 + (m2 - m1) * h * 6;
 		}
-		if ((h * 2f) < 1f) {
+		if ((h * 2) < 1) {
 			return m2;
 		}
-		if ((h * 3f) < 2f) {
-			return m1 + (m2 - m1) * ((2f / 3f) - h) * 6f;
+		if ((h * 3) < 2) {
+			return m1 + (m2 - m1) * ((2f / 3) - h) * 6;
 		}
 		return m1;
 	}
