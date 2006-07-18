c36b7f12930a722ca19480b89bd41a65bffa8cca
Flemming N Larsen <flemming.n.larsen@gmail.com>
2006-07-18 11:21:55 +0000

    Added option antialiasing, text antialiasing, rendering method, and method for getting the combined rendering hints

--- robocode/robocode/manager/RobocodeProperties.java
+++ robocode/robocode/manager/RobocodeProperties.java
@@ -9,7 +9,8 @@
  *     Mathew A. Nelson
  *     - Initial API and implementation
  *     Flemming N. Larsen
- *     - Added option for viewing ground
+ *     - Added option for viewing ground, antialiasing, text antialiasing,
+ *       rendering method, and method for getting the combined rendering hints
  *******************************************************************************/
 package robocode.manager;
 
@@ -17,6 +18,8 @@ package robocode.manager;
 import java.io.*;
 import java.util.*;
 import java.text.*;
+import java.awt.RenderingHints;
+
 import robocode.util.Utils;
 
 
@@ -33,12 +36,15 @@ public class RobocodeProperties {
 	private boolean optionsViewRobotEnergy = true;
 	private boolean optionsViewGround = true;
 	private boolean optionsViewFps = true;
+	private int optionsRenderingAntialiasing = 0; // 0 = default, 1 = on, 2 = off 
+	private int optionsRenderingTextAntialiasing = 0; // 0 = default, 1 = on, 2 = off
+	private int optionsRenderingMethod = 0; // 0 = default, 1 = speed, 2 = quality
+	private int optionsRenderingNoBuffers = 2; // 1 = single buffering, 2 = double buffering, 3 = tripple buffering 
 	private int optionsBattleDesiredFps = 30;
 	private boolean optionsBattleAllowColorChanges = false;
 	private boolean optionsTeamShowTeamRobots = false;
 	private String optionsDevelopmentPath = "";
 	private String lastRunVersion = "";
-	
 	private Date versionChecked;
 	private long robotFilesystemQuota = 200000;
 	private long consoleQuota = 8192;
@@ -51,6 +57,10 @@ public class RobocodeProperties {
 	private final static String OPTIONS_VIEW_ROBOTENERGY = "robocode.options.view.robotEnergy";
 	private final static String OPTIONS_VIEW_GROUND = "robocode.options.view.ground";
 	private final static String OPTIONS_VIEW_FPS = "robocode.options.view.FPS";
+	private final static String OPTIONS_RENDERING_ANTIALIASING = "robocode.options.rendering.antialiasing";
+	private final static String OPTIONS_RENDERING_TEXT_ANTIALIASING = "robocode.options.rendering.text.antialiasing";
+	private final static String OPTIONS_RENDERING_METHOD = "robocode.options.rendering.method";
+	private final static String OPTIONS_RENDERING_NO_BUFFERS = "robocode.options.rendering.noBuffers";
 	private final static String OPTIONS_BATTLE_DESIREDFPS = "robocode.options.battle.desiredFPS";
 	private final static String OPTIONS_BATTLE_ALLOWCOLORCHANGES = "robocode.options.battle.allowColorChanges";
 	private final static String OPTIONS_TEAM_SHOWTEAMROBOTS = "robocode.options.team.showTeamRobots";
@@ -60,7 +70,10 @@ public class RobocodeProperties {
 	private final static String CONSOLE_QUOTA = "robocode.console.quota";
 	private final static String CPU_CONSTANT = "robocode.cpu.constant.1000";
 	private final static String LAST_RUN_VERSION = "robocode.version.lastrun";
+
 	private RobocodeManager manager;
+
+	private RenderingHints renderingHints = new RenderingHints(new HashMap()); 
 
 	public RobocodeProperties(RobocodeManager manager) {
 		this.manager = manager;
@@ -159,6 +172,142 @@ public class RobocodeProperties {
 	public void setOptionsViewFps(boolean optionsViewFps) {
 		this.optionsViewFps = optionsViewFps;
 		props.setProperty(OPTIONS_VIEW_FPS, "" + optionsViewFps);
+	}
+
+	/**
+	 * Gets the optionsRenderingAntialiasing.
+	 * 
+	 * @return Returns an int
+	 */
+	public int getOptionsRenderingAntialiasing() {
+		return optionsRenderingAntialiasing;
+	}
+
+	/**
+	 * Sets the optionsRenderingAntialiasing.
+	 * 
+	 * @param optionsRenderingAntialiasing The optionsRenderingAntialiasing to set
+	 */
+	public void setOptionsRenderingAntialiasing(int optionsRenderingAntialiasing) {
+		this.optionsRenderingAntialiasing = optionsRenderingAntialiasing;
+		props.setProperty(OPTIONS_RENDERING_ANTIALIASING, "" + optionsRenderingAntialiasing);
+
+		Object value;
+
+		switch (optionsRenderingAntialiasing) {
+		case 1:
+			value = RenderingHints.VALUE_ANTIALIAS_ON;
+			break;
+
+		case 2:
+			value = RenderingHints.VALUE_ANTIALIAS_OFF;
+			break;		
+
+		case 0:
+		default:
+			value = RenderingHints.VALUE_ANTIALIAS_DEFAULT;
+		}
+		renderingHints.put(RenderingHints.KEY_ANTIALIASING, value);
+	}
+
+	/**
+	 * Gets the optionsRenderingTextAntialiasing.
+	 * 
+	 * @return Returns an int
+	 */
+	public int getOptionsRenderingTextAntialiasing() {
+		return optionsRenderingTextAntialiasing;
+	}
+
+	/**
+	 * Sets the optionsRenderingTextAntialiasing.
+	 * 
+	 * @param optionsRenderingTextAntialiasing The optionsRenderingTextAntialiasing to set
+	 */
+	public void setOptionsRenderingTextAntialiasing(int optionsRenderingTextAntialiasing) {
+		this.optionsRenderingTextAntialiasing = optionsRenderingTextAntialiasing;
+		props.setProperty(OPTIONS_RENDERING_TEXT_ANTIALIASING, "" + optionsRenderingTextAntialiasing);
+
+		Object value;
+
+		switch (optionsRenderingTextAntialiasing) {
+		case 1:
+			value = RenderingHints.VALUE_TEXT_ANTIALIAS_ON;
+			break;
+
+		case 2:
+			value = RenderingHints.VALUE_TEXT_ANTIALIAS_OFF;
+			break;		
+
+		case 0:
+		default:
+			value = RenderingHints.VALUE_TEXT_ANTIALIAS_DEFAULT;
+		}
+		renderingHints.put(RenderingHints.KEY_TEXT_ANTIALIASING, value);
+	}
+
+	/**
+	 * Gets the optionsRenderingMethod.
+	 * 
+	 * @return Returns an int
+	 */
+	public int getOptionsRenderingMethod() {
+		return optionsRenderingMethod;
+	}
+
+	/**
+	 * Sets the optionsRenderingMethod.
+	 * 
+	 * @param optionsRenderingMethod The optionsRenderingMethod to set
+	 */
+	public void setOptionsRenderingMethod(int optionsRenderingMethod) {
+		this.optionsRenderingMethod = optionsRenderingMethod;
+		props.setProperty(OPTIONS_RENDERING_METHOD, "" + optionsRenderingMethod);
+
+		Object value;
+
+		switch (optionsRenderingMethod) {
+		case 1:
+			value = RenderingHints.VALUE_RENDER_QUALITY;
+			break;
+
+		case 2:
+			value = RenderingHints.VALUE_RENDER_SPEED;
+			break;		
+
+		case 0:
+		default:
+			value = RenderingHints.VALUE_RENDER_DEFAULT;
+		}
+		renderingHints.put(RenderingHints.KEY_RENDERING, value);
+	}
+
+	/**
+	 * Gets the combined rendering options as RenderingHints.
+	 * 
+	 * @return Returns an RenderingHints value
+	 */
+	public RenderingHints getRenderingHints() {
+		return renderingHints;
+	}
+
+	/**
+	 * Gets the optionsRenderingNoBuffers
+	 * 
+	 * @return Returns an int
+	 */
+	public int getOptionsRenderingNoBuffers() {
+		return optionsRenderingNoBuffers;
+	}
+
+	/**
+	 * Sets the optionsRenderingNoBuffers.
+	 * 
+	 * @param optionsRenderingNoBuffers The optionsRenderingNoBuffers to set
+	 */
+	public void setOptionsRenderingNoBuffers(int optionsRenderingNoBuffers) {
+		this.optionsRenderingNoBuffers = optionsRenderingNoBuffers;
+		props.setProperty(OPTIONS_RENDERING_NO_BUFFERS, "" + optionsRenderingNoBuffers);
 	}
 
 	/**
@@ -311,6 +460,10 @@ public class RobocodeProperties {
 		optionsViewRobotEnergy = Boolean.valueOf(props.getProperty(OPTIONS_VIEW_ROBOTENERGY, "true")).booleanValue();
 		optionsViewGround = Boolean.valueOf(props.getProperty(OPTIONS_VIEW_GROUND, "true")).booleanValue();
 		optionsViewFps = Boolean.valueOf(props.getProperty(OPTIONS_VIEW_FPS, "true")).booleanValue();
+		optionsRenderingAntialiasing = Integer.parseInt(props.getProperty(OPTIONS_RENDERING_ANTIALIASING, "0"));
+		optionsRenderingTextAntialiasing = Integer.parseInt(props.getProperty(OPTIONS_RENDERING_TEXT_ANTIALIASING, "0"));
+		optionsRenderingMethod = Integer.parseInt(props.getProperty(OPTIONS_RENDERING_METHOD, "0"));
+		optionsRenderingNoBuffers = Integer.parseInt(props.getProperty(OPTIONS_RENDERING_NO_BUFFERS, "2"));
 		optionsBattleDesiredFps = Integer.parseInt(props.getProperty(OPTIONS_BATTLE_DESIREDFPS, "30"));
 		optionsBattleAllowColorChanges = Boolean.valueOf(props.getProperty(OPTIONS_BATTLE_ALLOWCOLORCHANGES, "false")).booleanValue();
 		optionsTeamShowTeamRobots = Boolean.valueOf(props.getProperty(OPTIONS_TEAM_SHOWTEAMROBOTS, "false")).booleanValue();
