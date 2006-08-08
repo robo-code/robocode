cb2423a8fb09de3d8c852f1306cb8054a97a4644
Flemming N Larsen <flemming.n.larsen@gmail.com>
2006-08-08 20:51:25 +0000

    Deprecation of setColors(), where setBodyColor(), setGunColor(), and setRadarColor() must be used instead

--- robocode/robocode/_Robot.java
+++ robocode/robocode/_Robot.java
@@ -9,10 +9,14 @@
  *     Mathew A. Nelson
  *     - Initial API and implementation
  *     Flemming N. Larsen
+ *     - Deprecation of setColors(), where setBodyColor(), setGunColor(), and
+ *       setRadarColor() must be used instead
  *     - Code cleanup
  *******************************************************************************/
 package robocode;
 
+
+import java.awt.Color;
 
 import robocode.peer.RobotPeer;
 import robocode.exception.*;
@@ -93,7 +97,7 @@ public class _Robot {
 	}
 
 	/**
-	 * @deprecated use getRoundNum instead.
+	 * @deprecated use getRoundNum() instead.
 	 */
 	public int getBattleNum() {
 		if (peer != null) {
@@ -110,6 +114,39 @@ public class _Robot {
 	 * and will no longer function in the Robot class.
 	 */
 	public void setInterruptible(boolean interruptible) {}
+
+	/**
+	 * @deprecated use setBodyColor(Color), setGunColor(Color), setRadarColor(Color) instead.
+	 *
+	 * Call this method to set your robot's colors.
+	 * You may only call this method one time per battle.
+	 * A null indicates the default (blue-ish) color.
+	 * 
+	 * <PRE>
+	 * Example:
+	 *   // Don't forget to import java.awt.Color at the top...
+	 *   import java.awt.Color;
+	 * 
+	 *   public void run() {
+	 *     setColors(Color.black,Color.red,new Color(150,0,150));
+	 *   }
+	 * </PRE>
+	 * 
+	 * @param bodyColor Your robot's body color
+	 * @param gunColor Your robot's gun color
+	 * @param radarColor Your robot's radar color
+	 * @see java.awt.Color
+	 */
+	public void setColors(Color bodyColor, Color gunColor, Color radarColor) {
+		if (peer != null) {
+			peer.setCall();
+			peer.setBodyColor(bodyColor);
+			peer.setGunColor(gunColor);
+			peer.setRadarColor(radarColor);
+		} else {
+			uninitializedException("setColors");
+		}
+	}
 
 	/**
 	 * @deprecated This call is not used.
