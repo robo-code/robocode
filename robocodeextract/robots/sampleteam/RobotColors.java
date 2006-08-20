59fc485d8ae474c16104e0a241a66a5feff00624
Flemming N Larsen <flemming.n.larsen@gmail.com>
2006-08-20 21:27:48 +0000

    Rewritten/simplified + scan color and bullet color was added

--- robocodeextract/robots/sampleteam/RobotColors.java
+++ robocodeextract/robots/sampleteam/RobotColors.java
@@ -8,25 +8,9 @@ import java.awt.Color;
  * RobotColors - A serializable class to send Colors to teammates
  */
 public class RobotColors implements java.io.Serializable {
-	private Color bodyColor;
-	private Color gunColor;
-	private Color radarColor;
-	
-	public RobotColors(Color bodyColor, Color gunColor, Color radarColor) {
-		this.bodyColor = bodyColor;
-		this.gunColor = gunColor;
-		this.radarColor = radarColor;
-	}
-	
-	public Color getBodyColor() {
-		return bodyColor;
-	}
-
-	public Color getGunColor() {
-		return gunColor;
-	}
-
-	public Color getRadarColor() {
-		return radarColor;
-	}
+	public Color bodyColor;
+	public Color gunColor;
+	public Color radarColor;
+	public Color scanColor;
+	public Color bulletColor;
 }
