20b203609b058650f3959ede4ad0de96bfd9bb85
Flemming N Larsen <flemming.n.larsen@gmail.com>
2006-11-13 12:22:49 +0000

    Removed unused constructor

--- robocode/robocode/manager/NameManager.java
+++ robocode/robocode/manager/NameManager.java
@@ -39,8 +39,6 @@ public class NameManager implements Serializable {
 	private String uniqueVeryShortClassNameWithVersion;
 	private String uniqueShortClassNameWithVersion;
 
-	private NameManager() {}
-	
 	public NameManager(String className, String version, boolean developmentVersion, boolean isTeam) {
 		if (className == null) {
 			throw new NullPointerException("className cannot be null.");
