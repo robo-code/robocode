404129bc9a34819f0d616e6f61c18c912a3bd8b2
Flemming N Larsen <flemming.n.larsen@gmail.com>
2007-02-27 12:40:08 +0000

    Totally rewritten to contain the functionality for both the RankingDialog and ResultsDialog (code reuse)

--- robocode/robocode/dialog/RankingDialog.java
+++ robocode/robocode/dialog/RankingDialog.java
@@ -6,149 +6,200 @@
  * http://robocode.sourceforge.net/license/cpl-v10.html
  *
  * Contributors:
- *     Luis Crespo
- *     - Initial API and implementation
+ *     Luis Crespo & Mathew A. Nelson
+ *     - Original implementation
  *     Flemming N. Larsen
- *     - Integration
- *     - When this dialog is closed, the state of the check box for the Ranking
- *       Panel in the Options Menu is set to unchecked
- *     - The table height in the scroll panel is now automatically resized when
- *       repainted in the repaint thread
+ *     - Totally rewritten to contain the functionality for both the
+ *       RankingDialog and ResultsDialog (code reuse)
  *******************************************************************************/
 package robocode.dialog;
 
 
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Frame;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableColumn;
 
 import robocode.battle.BattleRankingTableModel;
+import robocode.battle.BattleResultsTableModel;
 import robocode.manager.RobocodeManager;
 
 
 /**
- * Dialog to display the running ranking of a battle
+ * Dialog to display the battle results or ranking during battles.
  *
+ * @author Mathew A. Nelson (original)
  * @author Luis Crespo (original)
  * @author Flemming N. Larsen (contributor)
  */
 @SuppressWarnings("serial")
 public class RankingDialog extends JDialog {
-	private JPanel rankingContentPane;
-	private JScrollPane resultsScrollPane;
-	private JTable resultsTable;
+	private JPanel contentPane;
+	private JScrollPane scrollPane;
+	private JTable table;
+
+	private JPanel buttonPanel;
+	private JButton okButton;
+	private JButton saveButton;
+
 	private RobocodeManager manager;
-	private BattleRankingTableModel rankingTableModel;
+
+	private AbstractTableModel tableModel;
+
+	private Dimension tableSize;
+
 	private Thread thread;
+
+	private EventHandler eventHandler = new EventHandler();
+
+	private boolean isCurrentRankings;
 
 	/**
 	 * RankingDialog constructor
 	 */
-	public RankingDialog(Frame owner, RobocodeManager manager) {
+	public RankingDialog(Frame owner, RobocodeManager manager, boolean isCurrentRankings) {
 		super(owner);
 		this.manager = manager;
+		this.isCurrentRankings = isCurrentRankings;
 		initialize();
 	}
 
 	/**
-	 * Initializes the class
+	 * Initializes the dialog
 	 */
 	private void initialize() {
-		setTitle("Ranking");
+		setTitle(isCurrentRankings ? "Ranking" : ((BattleResultsTableModel) getTableModel()).getTitle());
 		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		setContentPane(getRankingContentPane());
+		setContentPane(getDialogContentPane());
 
-		addWindowListener(
-				new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent e) {
-				manager.getWindowManager().getRobocodeFrame().getRobocodeMenuBar().getOptionsShowRankingCheckBoxMenuItem().setState(
-						false);
-			}
-		});
+		addComponentListener(eventHandler);
 
-		addComponentListener(new ComponentAdapter() {
-			@Override
-			public void componentShown(ComponentEvent e) {
-				if (e.getSource() == RankingDialog.this) {
-					startRepaintThread();
+		if (isCurrentRankings) {
+			addWindowListener(
+					new WindowAdapter() {
+				@Override
+				public void windowClosing(WindowEvent e) {
+					manager.getWindowManager().getRobocodeFrame().getRobocodeMenuBar().getOptionsShowRankingCheckBoxMenuItem().setState(
+							false);
 				}
-			}
-
-			@Override
-			public void componentHidden(ComponentEvent e) {
-				if (e.getSource() == RankingDialog.this) {
-					stopRepaintThread();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Return the resultsTable.
-	 *
-	 * @return JTable
-	 */
-	private JTable getResultsTable() {
-		if (resultsTable == null) {
-			resultsTable = new JTable();
-			resultsTable.setColumnSelectionAllowed(true);
-			resultsTable.setRowSelectionAllowed(true);
-			resultsTable.getTableHeader().setReorderingAllowed(false);
-			setResultsData();
+			});
 		}
-		return resultsTable;
 	}
 
 	/**
-	 * Return the JDialogContentPane property value.
+	 * Return the content pane.
 	 *
 	 * @return JPanel
 	 */
-	private JPanel getRankingContentPane() {
-		if (rankingContentPane == null) {
-			rankingContentPane = new JPanel();
-			rankingContentPane.setLayout(new BorderLayout());
-			rankingContentPane.add(getRankingScrollPane(), "Center");
+	private JPanel getDialogContentPane() {
+		if (contentPane == null) {
+			contentPane = new JPanel();
+			contentPane.setLayout(new BorderLayout());
+			contentPane.add(getScrollPane(), "Center");
+			if (!isCurrentRankings) {
+				contentPane.add(getButtonPanel(), "South");
+			}
 		}
-		return rankingContentPane;
+		return contentPane;
 	}
 
 	/**
-	 * Return the rankingScrollPane
+	 * Return the buttonPanel.
+	 *
+	 * @return JPanel
+	 */
+	private JPanel getButtonPanel() {
+		if (buttonPanel == null) {
+			buttonPanel = new JPanel();
+			buttonPanel.setLayout(new BorderLayout());
+			buttonPanel.add(getOkButton(), "East");
+			buttonPanel.add(getSaveButton(), "West");
+		}
+		return buttonPanel;
+	}
+
+	/**
+	 * Return the okButton
+	 *
+	 * @return JButton
+	 */
+	private JButton getOkButton() {
+		if (okButton == null) {
+			okButton = new JButton();
+			okButton.setText("OK");
+			okButton.addActionListener(eventHandler);
+			WindowUtil.setFixedSize(okButton, new Dimension(80, 25));
+		}
+		return okButton;
+	}
+
+	/**
+	 * Return the saveButton
+	 *
+	 * @return JButton
+	 */
+	private JButton getSaveButton() {
+		if (saveButton == null) {
+			saveButton = new JButton();
+			saveButton.setText("Save");
+			saveButton.addActionListener(eventHandler);
+			WindowUtil.setFixedSize(saveButton, new Dimension(80, 25));
+		}
+		return saveButton;
+	}
+
+	/**
+	 * Return the scroll pane
 	 *
 	 * @return JScrollPane
 	 */
-	private JScrollPane getRankingScrollPane() {
-		if (resultsScrollPane == null) {
-			resultsScrollPane = new JScrollPane();
-			resultsScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-			resultsScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-			resultsScrollPane.getViewport().setScrollMode(JViewport.BLIT_SCROLL_MODE);
-			resultsScrollPane.setViewportView(getResultsTable());
-			resultsScrollPane.setColumnHeaderView(resultsTable.getTableHeader());
+	private JScrollPane getScrollPane() {
+		if (scrollPane == null) {
+			scrollPane = new JScrollPane();
+			scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+			scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+			scrollPane.getViewport().setScrollMode(JViewport.BLIT_SCROLL_MODE);
+			scrollPane.setViewportView(getTable());
+			scrollPane.setColumnHeaderView(table.getTableHeader());
+			scrollPane.addComponentListener(eventHandler);
+
+			tableSize = new Dimension(getTable().getColumnModel().getTotalColumnWidth(),
+					getTable().getModel().getRowCount() * (getTable().getRowHeight()));
+			table.setPreferredScrollableViewportSize(tableSize);
+			table.setPreferredSize(tableSize);
+			table.setMinimumSize(tableSize);
 		}
-		return resultsScrollPane;
+		return scrollPane;
+	}
+
+	/**
+	 * Return the table.
+	 *
+	 * @return JTable
+	 */
+	private JTable getTable() {
+		if (table == null) {
+			table = new JTable();
+			table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+			table.setColumnSelectionAllowed(true);
+			table.setRowSelectionAllowed(true);
+			table.getTableHeader().setReorderingAllowed(false);
+			setResultsData();
+		}
+		return table;
 	}
 
 	private void setResultsData() {
-		getResultsTable().setModel(getBattleRankingTableModel());
+		getTable().setModel(getTableModel());
 		int maxScoreColWidth = 0;
 
-		// TODO: the "name" column is getting too much width compared with the two others
-		// ...Columns should get a fixed width instead
-		for (int x = 0; x < getBattleRankingTableModel().getColumnCount(); x++) {
+		for (int x = 0; x < getTableModel().getColumnCount(); x++) {
 			if (x != 1) {
-				getResultsTable().getColumnModel().getColumn(x).setCellRenderer(new ResultsTableCellRenderer(false));
+				getTable().getColumnModel().getColumn(x).setCellRenderer(new ResultsTableCellRenderer(false));
 			}
-			TableColumn column = getResultsTable().getColumnModel().getColumn(x);
+			TableColumn column = getTable().getColumnModel().getColumn(x);
 			Component comp = null;
 
 			column.setHeaderRenderer(new ResultsTableCellRenderer(true));
@@ -156,30 +207,32 @@ public class RankingDialog extends JDialog {
 					0, 0);
 			int width = comp.getPreferredSize().width;
 
-			for (int y = 0; y < getBattleRankingTableModel().getRowCount(); y++) {
-				comp = getResultsTable().getDefaultRenderer(getBattleRankingTableModel().getColumnClass(x)).getTableCellRendererComponent(
-						getResultsTable(), getBattleRankingTableModel().getValueAt(y, x), false, false, 0, x);
+			for (int y = 0; y < getTableModel().getRowCount(); y++) {
+				comp = getTable().getDefaultRenderer(getTableModel().getColumnClass(x)).getTableCellRendererComponent(
+						getTable(), getTableModel().getValueAt(y, x), false, false, 0, x);
 				if (comp.getPreferredSize().width > width) {
 					width = comp.getPreferredSize().width;
 				}
 			}
-			TableColumn col = getResultsTable().getColumnModel().getColumn(x);
+			TableColumn col = getTable().getColumnModel().getColumn(x);
 
 			col.setPreferredWidth(width);
 			col.setMinWidth(width);
 			col.setWidth(width);
 
-			if (x >= 2 && width > maxScoreColWidth) {
+			if (x >= 3 && width > maxScoreColWidth) {
 				maxScoreColWidth = width;
 			}
 		}
 	}
 
-	private BattleRankingTableModel getBattleRankingTableModel() {
-		if (rankingTableModel == null) {
-			rankingTableModel = new BattleRankingTableModel(manager);
+	private AbstractTableModel getTableModel() {
+		if (tableModel == null) {
+			tableModel = isCurrentRankings
+					? new BattleRankingTableModel(manager)
+					: new BattleResultsTableModel(manager.getBattleManager().getBattle());
 		}
-		return rankingTableModel;
+		return tableModel;
 	}
 
 	private void startRepaintThread() {
@@ -192,8 +245,8 @@ public class RankingDialog extends JDialog {
 							sleep(1000);
 						} catch (InterruptedException e) {}
 
-						resultsTable.setSize(resultsTable.getColumnModel().getTotalColumnWidth(),
-								resultsTable.getModel().getRowCount() * resultsTable.getRowHeight());
+						table.setSize(table.getColumnModel().getTotalColumnWidth(),
+								table.getModel().getRowCount() * table.getRowHeight());
 
 						repaint();
 					}
@@ -205,5 +258,64 @@ public class RankingDialog extends JDialog {
 
 	private void stopRepaintThread() {
 		thread = null;
+	}
+
+	private class EventHandler implements ActionListener, ComponentListener {
+		public void actionPerformed(ActionEvent e) {
+			Object source = e.getSource();
+
+			if (source == RankingDialog.this.getOkButton()) {
+				okButtonActionPerformed();
+			} else if (source == RankingDialog.this.getSaveButton()) {
+				saveButtonActionPerformed();
+			}
+		}
+
+		public void componentShown(ComponentEvent e) {
+			if (e.getSource() == RankingDialog.this) {
+				startRepaintThread();
+			}
+		}
+
+		public void componentHidden(ComponentEvent e) {
+			if (e.getSource() == RankingDialog.this) {
+				stopRepaintThread();
+			}
+		}
+
+		public void componentResized(ComponentEvent e) {
+			if (e.getSource() == RankingDialog.this.getScrollPane()) {
+				scrollPaneComponentResized(e);
+			}
+		}
+
+		public void componentMoved(ComponentEvent e) {}
+	}
+
+	private void scrollPaneComponentResized(ComponentEvent componentEvent) {
+		// This code is not working...
+		Dimension scrollPaneExtent = getScrollPane().getViewport().getExtentSize();
+
+		if (tableSize != null && (tableSize.width < scrollPaneExtent.width)) {
+			getTable().setAutoResizeMode(JTable.AUTO_RESIZE_SUBSEQUENT_COLUMNS);
+			getTable().setSize(scrollPaneExtent);
+			getTable().sizeColumnsToFit(-1);
+		} else {
+			if (tableSize != null) {
+				getTable().setSize(tableSize);
+				getTable().sizeColumnsToFit(-1);
+			}
+			table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		}
+		validate();
+		repaint();
+	}
+
+	private void okButtonActionPerformed() {
+		dispose();
+	}
+
+	private void saveButtonActionPerformed() {
+		manager.getWindowManager().showSaveResultsDialog();
 	}
 }
