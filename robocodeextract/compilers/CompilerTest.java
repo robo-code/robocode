0079e2a576e6a1d0ba31210d8d92be38194d7c70
No Author <no_author@gmail.com>
2007-03-12 21:32:35 +0000

    This commit was manufactured by cvs2svn to create tag 'INITIAL_BETA9_B12'.

--- robocodeextract/compilers/CompilerTest.java
+++
@@ -1,5 +0,0 @@
-public class CompilerTest {
-	public static void main(String args[]) {
-		System.out.println("Compiler operational");
-	}
-}
--- robocodeextract/robocode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-olddir=`pwd` 
-robohome=`dirname $0` 
-echo "Using robohome $robohome" 
-cd "$robohome" 
-java -Xmx512M -Dsun.io.useCanonCaches=false -jar robocode.jar 
-echo "Goodbye!" 
-cd "$olddir"
