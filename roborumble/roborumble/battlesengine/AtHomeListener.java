433668ac5c11f2a247c2a973c6e2942a84cc7dd1
Flemming N Larsen <flemming.n.larsen@gmail.com>
2007-03-14 23:45:58 +0000

    Removed unused imports

--- roborumble/roborumble/battlesengine/AtHomeListener.java
+++ roborumble/roborumble/battlesengine/AtHomeListener.java
@@ -1,32 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2003, 2007 Albert Pérez and RoboRumble contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://robocode.sourceforge.net/license/cpl-v10.html
+ *
+ * Contributors:
+ *     Albert Pérez
+ *     - Initial API and implementation
+ *     Flemming N. Larsen
+ *     - Removed unused imports
+ *******************************************************************************/
 package roborumble.battlesengine;
 
 
 import robocode.control.*;
 import roborumble.battlesengine.Coordinator;
-import java.io.*;
-
-import apv.TeamCompetition;
 
 
 /**
  * TeamsListener by Albert Perez
  */
-
 public class AtHomeListener implements RobocodeListener {
-	Coordinator coord = null;
+	private Coordinator coord;
 
 	public AtHomeListener(Coordinator c) {
 		coord = c;
 	}
 
-	public void battleComplete(BattleSpecification battlespecification, RobotResults[] robotresultses) {
+	public void battleComplete(BattleSpecification battleSpec, RobotResults[] results) {
 		if (coord != null) {
 			coord.put();
 		}
 	}
     
-	public void battleAborted(BattleSpecification battlespecification) {}
+	public void battleAborted(BattleSpecification battleSpec) {}
     
 	public void battleMessage(String string) {}
-	
 }
