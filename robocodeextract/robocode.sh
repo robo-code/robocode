d11d28945e7bb280d956d5bb0162dbbfa36d9581
Flemming N Larsen <flemming.n.larsen@gmail.com>
2007-03-24 11:14:43 +0000

    The codesize.jar has been added to the classpath

--- robocodeextract/robocode.sh
+++ robocodeextract/robocode.sh
@@ -3,6 +3,6 @@ olddir=`pwd`
 robohome=`dirname $0` 
 echo "Using robohome $robohome" 
 cd "$robohome" 
-java -Xmx512M -Dsun.io.useCanonCaches=false -jar libs/robocode.jar 
+java -Xmx512M -Dsun.io.useCanonCaches=false -jar libs/robocode.jar:libs/codesize.jar
 echo "Goodbye!" 
 cd "$olddir"
