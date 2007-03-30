5e4abb4996f6ae626d2d547fd6eda66b29cef303
Flemming N Larsen <flemming.n.larsen@gmail.com>
2007-03-30 22:31:55 +0000

    Bugfixed: Robocode did not startup

--- robocodeextract/robocode.bat
+++ robocodeextract/robocode.bat
@@ -1,1 +1,1 @@
-java -Xmx512M -Dsun.io.useCanonCaches=false -jar libs/robocode.jar;libs/codesize.jar
+java -Xmx512M -Dsun.io.useCanonCaches=false -cp libs/codesize.jar -jar libs/robocode.jar
--- robocodeextract/robocode.sh
+++ robocodeextract/robocode.sh
@@ -3,6 +3,6 @@ olddir=`pwd`
 robohome=`dirname $0` 
 echo "Using robohome $robohome" 
 cd "$robohome" 
-java -Xmx512M -Dsun.io.useCanonCaches=false -jar libs/robocode.jar:libs/codesize.jar
+java -Xmx512M -Dsun.io.useCanonCaches=false -cp libs/codesize.jar -jar libs/robocode.jar
 echo "Goodbye!" 
 cd "$olddir"
