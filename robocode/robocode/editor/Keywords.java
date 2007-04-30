c4eb452e526ad1ebd769d01530906d4cbe5db596
Flemming N Larsen <flemming.n.larsen@gmail.com>
2007-04-30 22:23:55 +0000

    This class was made final The constructor was removed The string constant 'keywords' was renamed into 'KEYWORDS'

--- robocode/robocode/editor/Keywords.java
+++ robocode/robocode/editor/Keywords.java
@@ -8,6 +8,10 @@
  * Contributors:
  *     Mathew A. Nelson
  *     - Initial API and implementation
+ *     Flemming N. Larsen
+ *     - This class was made final
+ *     - The constructor was removed
+ *     - The string constant 'keywords' was renamed into 'KEYWORDS'
  *******************************************************************************/
 package robocode.editor;
 
@@ -17,10 +21,11 @@ import javax.swing.text.Segment;
 
 /**
  * @author Mathew A. Nelson (original)
+ * @author Flemming N. Larsen (contributor)
  */
-public class Keywords {
+public final class Keywords {
 	// TODO: Must be updated for Java 5?
-	public final static String keywords[] = {
+	private final static String KEYWORDS[] = {
 		"abstract", "boolean", "break", "byte", "case", "catch", "char", "class", "const", "continue", "default", "do",
 		"double", "else", "extends", "final", "finally", "float", "for", "goto", "if", "implements", "import",
 		"instanceof", "int", "interface", "long", "native", "new", "package", "private", "protected", "public", "return",
@@ -28,21 +33,14 @@ public class Keywords {
 		"void", "volatile", "while", "true", "false"
 	};
 
-	/**
-	 * Keywords constructor comment.
-	 */
-	public Keywords() {
-		super();
-	}
-
 	public static boolean isKeyword(Segment seg) {
 		boolean match = false;
 
-		for (int i = 0; !match && i < keywords.length; i++) {
-			if (seg.count == keywords[i].length()) {
+		for (int i = 0; !match && i < KEYWORDS.length; i++) {
+			if (seg.count == KEYWORDS[i].length()) {
 				match = true;
 				for (int j = 0; match && j < seg.count; j++) {
-					if (seg.array[seg.offset + j] != keywords[i].charAt(j)) {
+					if (seg.array[seg.offset + j] != KEYWORDS[i].charAt(j)) {
 						match = false;
 					}
 				}
