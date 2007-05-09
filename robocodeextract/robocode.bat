4219aae1ae4c9c731d1b458d8804aef157aeb9ac
Flemming N Larsen <flemming.n.larsen@gmail.com>
2007-05-09 21:14:15 +0000

    Bugfix: Codesize was not found

--- robocodeextract/robocode.bat
+++ robocodeextract/robocode.bat
@@ -1,1 +1,1 @@
-java -Xmx512M -Dsun.io.useCanonCaches=false -cp libs/codesize.jar -jar libs/robocode.jar
+java -Xmx512M -Dsun.io.useCanonCaches=false -cp libs/robocode.jar;libs/codesize.jar robocode.Robocode
