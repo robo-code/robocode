64985884e1f22b20be8bd95067e1691609a70c44
Flemming N Larsen <flemming.n.larsen@gmail.com>
2007-06-09 05:29:55 +0000

    Bugfix: Robocode ignored command parameters

--- robocodeextract/robocode.sh
+++ robocodeextract/robocode.sh
@@ -3,6 +3,6 @@ olddir=`pwd`
 robohome=`dirname $0` 
 echo "Using robohome $robohome" 
 cd "$robohome" 
-java -Xmx512M -Dsun.io.useCanonCaches=false -cp libs/robocode.jar:libs/codesize.jar robocode.Robocode
+java -Xmx512M -Dsun.io.useCanonCaches=false -cp libs/robocode.jar:libs/codesize.jar robocode.Robocode $*
 echo "Goodbye!" 
 cd "$olddir"
