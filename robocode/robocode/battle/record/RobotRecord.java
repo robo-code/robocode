c7b4cf3d8bd80fab7e224d619523ad23cf1d66e8
Flemming N Larsen <flemming.n.larsen@gmail.com>
2007-09-24 19:40:06 +0000

    - Moved the toRGB565 into the robocode.gfx.ColorUtil class - The bulletColor has been moved to the BulletRecord class

--- robocode/robocode/battle/record/RobotRecord.java
+++ robocode/robocode/battle/record/RobotRecord.java
@@ -12,6 +12,8 @@
 package robocode.battle.record;
 
 
+import static robocode.gfx.ColorUtil.toRGB565;
+
 import robocode.peer.RobotPeer;
 
 
@@ -55,9 +57,6 @@ public class RobotRecord {
 	// Radar color packed as RGB 565, where 0 means null
 	public short radarColor;
 
-	// Bullet color packed as RGB 565, where 0 means null
-	public short bulletColor;
-
 	// Scan color packed as RGB 565, where 0 means null
 	public short scanColor;
 
@@ -79,24 +78,6 @@ public class RobotRecord {
 		bodyColor = toRGB565(robot.getBodyColor());
 		gunColor = toRGB565(robot.getGunColor());
 		radarColor = toRGB565(robot.getRadarColor());
-		bulletColor = toRGB565(robot.getBulletColor());
 		scanColor = toRGB565(robot.getScanColor());
-	}
-
-	private static short toRGB565(java.awt.Color c) {
-		if (c == null) {
-			return 0;
-		}
-		short rgb = (short) (((c.getRed() & 0xf8) << 8) | ((c.getGreen() & 0xfc) << 3) | (c.getBlue() >> 3));
-
-		// 0 is reserved for null -> set green (has highest resolution) to 1
-		if (rgb == 0) {
-			return 0x20;
-		}
-		// If the color actually was 0x20 then set it to 0x40 (to the nearest green)
-		if (rgb == 0x20) {
-			return 0x40;
-		}
-		return rgb;
 	}
 }
