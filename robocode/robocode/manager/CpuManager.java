12cef090dc1e59e0c1f19c9af20306f348bf8cc8
Flemming N Larsen <flemming.n.larsen@gmail.com>
2007-11-22 22:16:42 +0000

    Went back to use 1000 (Robocode 1.0.7) cycles instead of 50.000. Now the CPU constant is measured in nanoseconds rather than milliseconds. Removed the getMillisGranularity() method.

--- robocode/robocode/manager/CpuManager.java
+++ robocode/robocode/manager/CpuManager.java
@@ -9,17 +9,14 @@
  *     Mathew A. Nelson
  *     - Initial API and implementation
  *     Flemming N. Larsen
- *     - Changed to accept 50000 cycles instead of 1000 (introduced in v1.07),
- *       which caused too many skipped turns on existing bots that ran just fine
- *       under v1.06.
  *     - Updated to use methods from WindowUtil and Logger, which replaces
  *       methods that have been (re)moved from the robocode.util.Utils class
  *     - Added calculateCpuConstant() used for (re)calculating the CPU constant
  *     - Added setCpuConstant() for calculating and setting the CPU constant
- *     - Added getMillisGranularity() for calculating the time millis
- *       granularity
  *     - Limited the CPU constant to be >= millis granularity in order to prevent
  *       robots from skipping turns as the granularity might be coarse
+ *     - Changed CPU constant to be measured in nanoseconds instead of
+ *       milliseconds
  *******************************************************************************/
 package robocode.manager;
 
@@ -33,17 +30,17 @@ import robocode.io.Logger;
  * @author Flemming N. Larsen (contributor)
  */
 public class CpuManager {
-	private final static int APPROXIMATE_CYCLES_ALLOWED = 50000;
-	private final static int TEST_PERIOD_MILLIS = 5000;
+	private final static int APPROXIMATE_CYCLES_ALLOWED = 1000;
+	private final static long TEST_PERIOD_NANOSECS = 1000000000L; // 1 second
 
-	private int cpuConstant = -1;
+	private long cpuConstant = -1;
 	private RobocodeManager manager;
 
 	public CpuManager(RobocodeManager manager) {
 		this.manager = manager;
 	}
 
-	public int getCpuConstant() {
+	public long getCpuConstant() {
 		if (cpuConstant == -1) {
 			cpuConstant = manager.getProperties().getCpuConstant();
 			if (cpuConstant == -1) {
@@ -58,7 +55,7 @@ public class CpuManager {
 
 		setCpuConstant();
 
-		Logger.log("Each robot will be allowed a maximum of " + cpuConstant + " milliseconds per turn on this system.");
+		Logger.log("Each robot will be allowed a maximum of " + cpuConstant + " nanoseconds per turn on this system.");
 		manager.getProperties().setCpuConstant(cpuConstant);
 		manager.saveProperties();
 
@@ -66,40 +63,15 @@ public class CpuManager {
 	}
 	
 	private void setCpuConstant() {
-		long start = System.currentTimeMillis();
+		long start = System.nanoTime();
 		long count = 0;
 		double d = 0;
 
-		while (System.currentTimeMillis() - start < TEST_PERIOD_MILLIS && d >= 0) {
+		while (System.nanoTime() - start < TEST_PERIOD_NANOSECS && d >= 0) {
 			d = Math.random() * Math.random();
 			count++;
 		}
 
-		double cyclesPerMS = count / (double) TEST_PERIOD_MILLIS;
-
-		double msPerCycle = 1 / cyclesPerMS;
-
-		cpuConstant = Math.max((int) (APPROXIMATE_CYCLES_ALLOWED * msPerCycle + .5), getMillisGranularity());
-
-		if (cpuConstant < 1) {
-			cpuConstant = 1;
-		}
-	}
-
-	private int getMillisGranularity() {
-		int granularity = 0;
-		int delta = 0, i;
-
-		// Run several times, so we find the most representative granularity
-		// Sometimes, e.g. 1 out of 100 times we get a lower granularity.
-		for (i = 0; i < 10; i++) {
-			long time = System.currentTimeMillis();
-
-			while ((delta = (int) (System.currentTimeMillis() - time)) == 0);
-			
-			granularity = Math.max(granularity, delta);
-		}
-
-		return granularity;
+		cpuConstant = Math.max(1, APPROXIMATE_CYCLES_ALLOWED * TEST_PERIOD_NANOSECS / count);
 	}
 }
