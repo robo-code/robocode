0a5b8b8d704c7f8abbc7a495eda6bd2ed9e498f9
Pavel Savara <pavel.savara@gmail.com>
2008-02-04 07:45:56 +0000

    VS Express starter

---
+++ nrobocode/Starter.cs
@@ -0,0 +1,28 @@
+﻿// ****************************************************************************
+// Copyright (c) 2001, 2008 Mathew A. Nelson and Robocode contributors
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Common Public License v1.0
+// which accompanies this distribution, and is available at
+// http://robocode.sourceforge.net/license/cpl-v10.html
+// 
+// Contributors:
+// Pavel Savara
+// - Initial implementation
+// *****************************************************************************
+
+using System;
+
+namespace nrobocode
+{
+    /// <summary>
+    /// This is helper class to dumb VS Express
+    /// </summary>
+    class Starter
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            robocode.Robocode.main(args);
+        }
+    }
+}
