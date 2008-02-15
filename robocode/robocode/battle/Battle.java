fe4d1d38b3691c73a14e63d837a22ef7bb1590b3
Flemming N Larsen <flemming.n.larsen@gmail.com>
2008-02-15 22:28:09 +0000

    Bugfix: The CPU constant was way too little compared to version 1.4.9. Now a improved "heavy math" algorithm is used for calculating the CPU constant, and the RobotPeer.wait part was optimized to use nano second granularity.

--- robocode/robocode/battle/Battle.java
+++ robocode/robocode/battle/Battle.java
@@ -81,6 +81,11 @@
  *       been extended to cleanup all robots, but also all classes that this
  *       class refers to in order to avoid circular references. In addition,
  *       cleanup has been added to the KeyEventHandler
+ *     Julian Kent
+ *     - Fix: Method for using only nano second precision when using
+ *       RobotPeer.wait(0, nanoSeconds) in order to prevent the millisecond
+ *       granularity issue, which is typically were coarse compared to the one
+ *       with nano seconds 
  *******************************************************************************/
 package robocode.battle;
 
@@ -130,6 +135,7 @@ import robocode.security.RobocodeClassLoader;
  * @author Robert D. Maupin (contributor)
  * @author Titus Chen (contributor)
  * @author Nathaniel Troutman (contributor)
+ * @author Julian Kent (contributor)
  */
 public class Battle implements Runnable {
 
@@ -1152,9 +1158,15 @@ public class Battle implements Runnable {
 							try {
 								long waitTime = manager.getCpuManager().getCpuConstant();
 
-								r.wait(waitTime / 1000000, (int) (waitTime % 1000000));
+								int millisWait = (int) (waitTime / 1000000);
+
+								for (int i = millisWait; i > 0 && !r.isSleeping(); i--) {
+									r.wait(0, 999999);
+								}
+								if (!r.isSleeping()) {
+									r.wait(0, (int) (waitTime % 1000000));
+								}
 							} catch (InterruptedException e) {
-								// ?
 								log("Wait for " + r + " interrupted.");
 							}
 						}
