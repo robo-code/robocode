2079182bae43b6723ee18f237fbcb1b11a08f40f
Flemming N Larsen <flemming.n.larsen@gmail.com>
2008-03-06 22:10:07 +0000

    Fixed a security issue, where robot was able to access the internals of the Robocode game thru the AWT Event Queue.

--- robocode/robocode/manager/ThreadManager.java
+++ robocode/robocode/manager/ThreadManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2007 Mathew A. Nelson and Robocode contributors
+ * Copyright (c) 2001, 2008 Mathew A. Nelson and Robocode contributors
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,6 +11,8 @@
  *     Flemming N. Larsen
  *     - Code cleanup
  *     - Fixed potential NullPointerException in getLoadingRobotPeer()
+ *     - Added getRobotClasses() and getRobotPeers() for the
+ *       RobocodeSecurityManager
  *     Robert D. Maupin
  *     - Replaced old collection types like Vector and Hashtable with
  *       synchronized List and HashMap
@@ -22,6 +24,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import robocode._RobotBase;
 import robocode.peer.RobotPeer;
 
 
@@ -76,6 +79,8 @@ public class ThreadManager {
 	}
 
 	public void reset() {
+		robotLoaderThread = null;
+		loadingRobot = null;
 		groups.clear();
 		robots.clear();
 	}
@@ -88,5 +93,43 @@ public class ThreadManager {
 
 	public synchronized void setRobotLoaderThread(Thread robotLoaderThread) {
 		this.robotLoaderThread = robotLoaderThread;
+	}
+
+	public List<Class<?>> getRobotClasses() {
+		List<Class<?>> classes = new ArrayList<Class<?>>();
+
+		RobotPeer robotPeer;
+		_RobotBase robot;
+
+		for (int i = robots.size() - 1; i >= 0; i--) {
+			robotPeer = robots.get(i);
+			if (robotPeer != null) {
+				robot = robotPeer.getRobot();
+				if (robot != null) {
+					classes.add(robot.getClass());
+				}
+			}
+		}
+		return classes;
+	}
+
+	public List<RobotPeer> getRobotPeers(Class<?> robotClass) {
+		List<RobotPeer> robotPeers = new ArrayList<RobotPeer>();
+
+		for (int i = robots.size() - 1; i >= 0; i--) {
+			RobotPeer robotPeer = robots.get(i);
+
+			if (robotPeer != null) {
+				_RobotBase robot = robotPeer.getRobot();
+
+				// NOTE: The check is on name level, as the equals() method does not work between
+				// the two classes.
+				if (robot != null && robot.getClass().getName().equals(robotClass.getName())) {
+					robotPeers.add(robotPeer);
+				}
+			}
+		}
+
+		return robotPeers;
 	}
 }
