52de4dfa42d818f290b98fe6cf4b56ce47b7a6a7
Flemming N Larsen <flemming.n.larsen@gmail.com>
2008-04-10 22:07:45 +0000

    Removed the samplescale robot from 1.6.0, which is available in 1.6.1 instead

--- robocodeextract/robots/samplescala/package.cmd
+++
@@ -1,1 +0,0 @@
-jar -cf ..\..\..\build\build\robots\samplescala.MyFirstScalaRobot.jar -C ..\..\..\build\build\robots samplescala\MyFirstScalaRobot.class -C ..\..\..\build\build\robots samplescala\MyFirstScalaRobot.properties
