db20cfd65948eda8e884292e7b28a34b653a17c8
Pavel Savara <pavel.savara@gmail.com>
2008-04-19 14:50:41 +0000

    Fix for version manager started from IDE, when there is no jar to read version from. Fixes team creation dialog.

--- robocode/robocode/manager/VersionManager.java
+++ robocode/robocode/manager/VersionManager.java
@@ -186,16 +186,17 @@ public final class VersionManager {
 
 			if (versionsUrl == null) {
 				log("no url");
-				return null;
+                versionString = "unknown";
 			}
+            else {
+                in = new BufferedReader(new InputStreamReader(versionsUrl.openStream()));
 
-			in = new BufferedReader(new InputStreamReader(versionsUrl.openStream()));
-
-			versionString = in.readLine();
-			while (versionString != null && !versionString.substring(0, 8).equalsIgnoreCase("Version ")) {
-				versionString = in.readLine();
-			}
-		} catch (FileNotFoundException e) {
+                versionString = in.readLine();
+                while (versionString != null && !versionString.substring(0, 8).equalsIgnoreCase("Version ")) {
+                    versionString = in.readLine();
+                }
+            }
+        } catch (FileNotFoundException e) {
 			log("No versions.txt file in robocode.jar.");
 			versionString = "unknown";
 		} catch (IOException e) {
