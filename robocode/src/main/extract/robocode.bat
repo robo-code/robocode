b1d9619d5d569d2fdfd57f93028d6a248b25eb55
Pavel Savara <pavel.savara@gmail.com>
2008-04-29 23:07:37 +0000

    better package

--- robocode/src/main/extract/robocode.bat
+++ robocode/src/main/extract/robocode.bat
@@ -1,1 +1,1 @@
-java -Xmx512M -Dsun.io.useCanonCaches=false -cp libs/robocode.jar;libs/codesize.jar;libs/cachecleaner.jar robocode.Robocode
+java -Xmx512M -Dsun.io.useCanonCaches=false -cp libs/robocode-1.6.2.jar;libs/codesize-1.1.jar robocode.Robocode
