41e1a36e99a296dc294291c42fd35851e214b4e9
Flemming N Larsen <flemming.n.larsen@gmail.com>
2008-08-20 19:37:10 +0000

    Change: The intro battle will only be shown if a battle file has not been specified using the -battle command-line argument and Robocode is being run for the first time since installation. Previously, the intro battle was always shown even though a battle file had been specified.

--- robocode/robocode/Robocode.java
+++ robocode/robocode/Robocode.java
@@ -119,7 +119,8 @@ public class Robocode {
 					manager.getWindowManager().getRobocodeFrame().setState(Frame.ICONIFIED);
 				}
 
-				if (!manager.getProperties().getLastRunVersion().equals(manager.getVersionManager().getVersion())) {
+				// Play the intro battle if a battle file is not specified and this is the first time Robocode is being run
+				if (setup.battleFilename == null && !manager.getProperties().getLastRunVersion().equals(manager.getVersionManager().getVersion())) {
 					manager.getProperties().setLastRunVersion(manager.getVersionManager().getVersion());
 					manager.saveProperties();
 					manager.getWindowManager().getRobocodeFrame().runIntroBattle();
