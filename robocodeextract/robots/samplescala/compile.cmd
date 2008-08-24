846f77015d1157c359d125c63c583e60938039d1
Flemming N Larsen <flemming.n.larsen@gmail.com>
2008-08-24 10:28:47 +0000

    Removed samplescale package from trunk. Perhaps it should be moved to its own branch instead of th e main/trunk which will propagate into every other branch.

--- robocodeextract/robots/samplescala/compile.cmd
+++
@@ -1,3 +0,0 @@
-call "c:\Program Files\Scala\bin\scalac.bat" -cp ..\..\..\build\build\libs\robocode.jar MyFirstScalaRobot.scala -d ..\..\..\build\build\robots
-copy MyFirstScalaRobot.properties ..\..\..\build\build\robots\samplescala
-copy MyFirstScalaRobot.scala ..\..\..\build\build\robots\samplescala
