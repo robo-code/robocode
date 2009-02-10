e9a71378835ba8cc3b6dccf646585f664787741d
Flemming N Larsen <flemming.n.larsen@gmail.com>
2009-02-10 22:06:39 +0000

    Fixed jacobe.sh, which did not run correctly

--- jacobe.sh
+++ jacobe.sh
@@ -7,11 +7,11 @@ dp0=${0%/*}
 if [ ! -f $dp0/tools/lib/ant-launcher.jar ]; then
    $dp0/tools/loadTools.sh
 elif [ ! -f $dp0/tools/lib/ant.jar ]; then
-   $dp0/loadTools.sh
+   $dp0/tools/loadTools.sh
 elif [ ! -f $dp0/tools/lib/jacobe.jar ]; then
-   $dp0/loadTools.sh
+   $dp0/tools/loadTools.sh
 fi
 
-export CLASSPATH=$CLASSPATH:$dp0/tools/lib/ant-launcher.jar:$dp0/tools/lib/ant.jar:$dp0/lib/jacobe.jar
-$dp0/bin/ant.sh -buildfile $dp0/jacobe/build.xml
+export CLASSPATH=$CLASSPATH:$dp0/tools/lib/ant-launcher.jar:$dp0/tools/lib/ant.jar:$dp0/tools/lib/jacobe.jar
+$dp0/tools/bin/ant.sh -buildfile $dp0/tools/jacobe/build.xml
 
