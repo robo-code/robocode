559f1e1ffbb46d034c9e9c890f00b43739c5b459
Pavel Savara <pavel.savara@gmail.com>
2009-02-18 23:31:02 +0000

    - this is just frame for .NET host (there is not yet anything clever about it) - maven is able to compile C# now - therefor we can't use "Beta" in name of version, this needs better solution later

---
+++ plugins/dotnet/robocode.dotnet.repository/src/main/java/net/sf/robocode/dotnet/repository/items/handlers/DotNetPropertiesHandler.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2001, 2009 Mathew A. Nelson and Robocode contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://robocode.sourceforge.net/license/cpl-v10.html
+ *
+ * Contributors:
+ *     Pavel Savara
+ *     - Initial implementation
+ *******************************************************************************/
+package net.sf.robocode.dotnet.repository.items.handlers;
+
+
+import net.sf.robocode.repository.items.handlers.PropertiesHandler;
+import net.sf.robocode.repository.items.RobotItem;
+import net.sf.robocode.repository.root.IRepositoryRoot;
+import net.sf.robocode.repository.Database;
+import net.sf.robocode.dotnet.repository.items.DotNetRobotItem;
+
+import java.net.URL;
+
+
+/**
+ * @author Pavel Savara (original)
+ */
+public class DotNetPropertiesHandler extends PropertiesHandler {
+
+	@Override
+	protected RobotItem createItem(URL itemURL, IRepositoryRoot root, Database db) {
+		return new DotNetRobotItem(null, itemURL, root);
+	}
+}
