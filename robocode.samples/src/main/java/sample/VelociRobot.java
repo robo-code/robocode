169cddc3c75b5aa699b78999a368901e8e1d32a6
Flemming N Larsen <flemming.n.larsen@gmail.com>
2009-05-27 22:38:09 +0000

    Did some minor changes/adjustments on the RateControlRobot ant its TestRateControl test unit due to code review + code beautified with Jacobe. This new robot is in a good shape. :-)

--- robocode.samples/src/main/java/sample/VelociRobot.java
+++ robocode.samples/src/main/java/sample/VelociRobot.java
@@ -12,19 +12,20 @@
 
 package sample;
 
+
 import robocode.HitByBulletEvent;
 import robocode.HitWallEvent;
 import robocode.RateControlRobot;
 import robocode.ScannedRobotEvent;
 
+
 /**
  * This is a sample of a robot using the RateControlRobot class
  * 
  * @author Joshua Galecki
- *
  */
-public class VelociRobot extends RateControlRobot 
-{
+public class VelociRobot extends RateControlRobot {
+
 	int turnCounter;
 	public void run() {
 
@@ -32,16 +33,14 @@ public class VelociRobot extends RateControlRobot
 		setGunRotationRate(15);
 		
 		while (true) {
-			if (turnCounter % 64 == 0)
-			{
-				//Straighten out, if we were hit by a bullet and are turning
+			if (turnCounter % 64 == 0) {
+				// Straighten out, if we were hit by a bullet and are turning
 				setTurnRate(0);
-				//Go forward with a velocity of 4
+				// Go forward with a velocity of 4
 				setVelocityRate(4);
 			}
-			if (turnCounter % 64 == 32)
-			{
-				//Go backwards, faster
+			if (turnCounter % 64 == 32) {
+				// Go backwards, faster
 				setVelocityRate(-6);
 			}
 			turnCounter++;
@@ -54,12 +53,12 @@ public class VelociRobot extends RateControlRobot
 	}
 
 	public void onHitByBullet(HitByBulletEvent e) {
-		//Turn to confuse the other robot
+		// Turn to confuse the other robot
 		setTurnRate(5);
 	}
 	
 	public void onHitWall(HitWallEvent e) {
-		//Move away from the wall
+		// Move away from the wall
 		setVelocityRate(-1 * getVelocityRate());
 	}
 }
