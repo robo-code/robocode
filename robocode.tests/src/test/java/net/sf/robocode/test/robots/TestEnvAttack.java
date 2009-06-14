8525fb85c39eb18622664a37a2e00788a4332fd8
Pavel Savara <pavel.savara@gmail.com>
2009-06-14 15:53:50 +0000

    wrong file header

--- robocode.tests/src/test/java/net/sf/robocode/test/robots/TestEnvAttack.java
+++ robocode.tests/src/test/java/net/sf/robocode/test/robots/TestEnvAttack.java
@@ -1,20 +1,14 @@
-/* Copyright (C) 2009 by Pavel Savara
-This file is part of of jni4net - bridge between Java and .NET
-http://jni4net.sourceforge.net/
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/*******************************************************************************
+ * Copyright (c) 2001, 2009 Mathew A. Nelson and Robocode contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://robocode.sourceforge.net/license/cpl-v10.html
+ *
+ * Contributors:
+ *     Pavel Savara
+ *     - Initial implementation
+ *******************************************************************************/
 package net.sf.robocode.test.robots;
 
 import net.sf.robocode.test.helpers.Assert;
