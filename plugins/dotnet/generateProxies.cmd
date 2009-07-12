8db6a2d2a9ab8e0a424090f8beae0abbe8e8e6e0
Pavel Savara <pavel.savara@gmail.com>
2009-07-12 10:58:43 +0000

    upgrading to jni4net 0.2

--- plugins/dotnet/generateProxies.cmd
+++ plugins/dotnet/generateProxies.cmd
@@ -1,9 +1,4 @@
 @echo off
 
-cd robocode.dotnet.nhost
-..\tools\lib\jni4net.proxygen-0.1.0.0.exe src\robocode.proxygen.xml
-cd ..
+tools\lib\jni4net.proxygen-0.2.0.0.exe tools\proxygen\robocode.proxygen.xml
 
-rem cd robocode.dotnet.host
-rem ..\tools\lib\jni4net.proxygen-0.1.0.0.exe src\robocode.proxygen.xml
-rem cd ..
