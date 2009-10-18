fa81a4931cb36c7d993a33d742cf2caa92c61c4c
Pavel Savara <pavel.savara@gmail.com>
2009-10-18 15:25:57 +0000

    passed robot loading test

--- robocode.tests/src/main/java/net/sf/robocode/test/helpers/RobocodeTestBed.java
+++ robocode.tests/src/main/java/net/sf/robocode/test/helpers/RobocodeTestBed.java
@@ -48,7 +48,20 @@ public abstract class RobocodeTestBed extends BattleAdaptor {
 		System.setProperty("EXPERIMENTAL", "true");
 		System.setProperty("TESTING", "true");
 		System.setProperty("WORKINGDIRECTORY", "target//test-classes");
-        System.setProperty("ROBOTPATH", "../robocode.tests.robots/target/classes");
+        try {
+            if (new File("").getAbsolutePath().endsWith("robocode.tests")){
+                robotsPath = new File("../robocode.tests.robots").getCanonicalPath();
+            }
+            else if (new File("").getAbsolutePath().endsWith("robocode.dotnet.tests")){
+                robotsPath = new File("../../../robocode.tests.robots").getCanonicalPath();
+            } else{
+                throw new Error("Unknown directory");
+            }
+        } catch (IOException e) {
+            e.printStackTrace(Logger.realErr);
+        }
+        System.setProperty("ROBOTPATH", robotsPath +"/target/classes");
+
 		engine = new RobocodeEngine(new BattleAdaptor() {
 			public void onBattleMessage(BattleMessageEvent event) {
 				if (isDumpingMessages) {
@@ -64,11 +77,6 @@ public abstract class RobocodeTestBed extends BattleAdaptor {
 				errors++;
 			}
 		});
-		try {
-            robotsPath = new File("../robocode.tests.robots").getCanonicalPath();
-		} catch (IOException e) {
-			e.printStackTrace(Logger.realErr);
-		}
 	}
 
 	public RobocodeTestBed() {
