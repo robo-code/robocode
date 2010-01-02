8c87528edca3a7f1d433140ed204e7f8e80a1c24
Pavel Savara <pavel.savara@gmail.com>
2010-01-02 01:03:41 +0000

    building helpers

--- plugins/dotnet/mvnassembly.cmd
+++ plugins/dotnet/mvnassembly.cmd
@@ -1,6 +1,5 @@
 @echo off
-rem if exist "%~dp0\..\..\tools\lib\maven-2.0.9-uber.jar" goto mvn
-rem call "%~dp0\..\..\tools\loadTools.cmd"
-rem :mvn
-rem @call "%~dp0\..\..\tools\bin\mvn.bat" package assembly:assembly -Dmaven.test.skip=false
-mvn install assembly:assembly -Dmaven.test.skip=false
+if exist "%~dp0\..\..\tools\lib\maven-2.0.9-uber.jar" goto mvn
+call "%~dp0\..\..\tools\loadTools.cmd"
+:mvn
+@call "%~dp0..\..\tools\bin\mvn.bat" package assembly:assembly -Dmaven.test.skip=false
--- plugins/dotnet/tools/keys/gennetkey.cmd
+++ plugins/dotnet/tools/keys/gennetkey.cmd
@@ -1,1 +1,2 @@
-"C:\Program Files\Microsoft SDKs\Windows\v7.0A\bin\sn.exe" -k robocode.snk
+rem "C:\Program Files\Microsoft SDKs\Windows\v7.0A\bin\sn.exe" -k robocode.snk
+"C:\Program Files\Microsoft SDKs\Windows\v6.0A\Bin\sn.exe" -k robocode.snk
