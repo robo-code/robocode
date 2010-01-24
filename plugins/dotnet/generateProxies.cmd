cbd37b3f0ff3bec9c3e1208f4acdf2a5330eea2b
Pavel Savara <pavel.savara@gmail.com>
2010-01-24 14:24:33 +0000

    will upload it again

--- plugins/dotnet/generateProxies.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-tools\lib\jni4net.proxygen-0.2.0.0.exe tools\proxygen\robocode.proxygen.xml
-
--- plugins/dotnet/tools/loadTools.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-java -cp %~dp0/../../../tools/loader Loader http://jni4net.googlecode.com/svn/mvnrepo/net/sf/jni4net/jni4net.n/0.2.0.0/ %~dp0/lib/ jni4net.n-0.2.0.0.dll
-java -cp %~dp0/../../../tools/loader Loader http://jni4net.googlecode.com/svn/mvnrepo/net/sf/jni4net/jni4net.j/0.2.0.0/ %~dp0/lib/ jni4net.j-0.2.0.0.jar
-java -cp %~dp0/../../../tools/loader Loader http://jni4net.googlecode.com/svn/mvnrepo/net/sf/jni4net/jni4net.proxygen/0.2.0.0/ %~dp0/lib/ jni4net.proxygen-0.2.0.0.exe
-
