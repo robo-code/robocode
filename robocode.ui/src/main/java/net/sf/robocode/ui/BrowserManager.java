29d026e792329f0d2525d7ed9911d8b149c028fd
Flemming N Larsen <flemming.n.larsen@gmail.com>
2010-01-31 13:11:07 +0000

    Browser support has been improved for Mac OS, Unix and Linux, which in most cases did not work properly. In previous versions of Robocode, the browser.sh was used to start up the user's prefered browser. The browser.sh file is not being used anymore, and hence this file can safely be deleted from your robocode installation dir. Robocode will itself start up a browser under Mac OS, Unix and Linux.

--- robocode.ui/src/main/java/net/sf/robocode/ui/BrowserManager.java
+++ robocode.ui/src/main/java/net/sf/robocode/ui/BrowserManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2008 Mathew A. Nelson and Robocode contributors
+ * Copyright (c) 2001, 2010 Mathew A. Nelson and Robocode contributors
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -25,36 +25,34 @@ import java.io.IOException;
  */
 public class BrowserManager {
 
-	private static final String browserCommand;
-
-	static {
-		if (System.getProperty("os.name").toLowerCase().startsWith("windows")) {
-			browserCommand = "rundll32 url.dll, FileProtocolHandler";
-		} else {
-			browserCommand = "browser.sh";
-		}
-	}
-
 	public static void openURL(String url) throws IOException {
 		url = FileUtil.quoteFileName(url);
 
-		final String command = browserCommand + ' ' + url;
+		Runtime rt = Runtime.getRuntime();
+		String os = System.getProperty("os.name").toLowerCase();
 
-		ProcessBuilder pb = new ProcessBuilder(command.split(" "));
+		if (os.startsWith("windows")) {
+			rt.exec("rundll32 url.dll, FileProtocolHandler " + url);
 
-		pb.directory(FileUtil.getCwd());
-		Process p = pb.start();
+		} else if (os.startsWith("mac")) {
+			rt.exec("open " + url);
 
-		try {
-			p.waitFor();
-		} catch (InterruptedException e) {
-			// Immediately reasserts the exception by interrupting the caller thread itself
-			Thread.currentThread().interrupt();
-		}
+		} else if (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0) {
+			// Do a best guess on Unix until we get a platform independent way.
+			// Build a list of browsers to try, in this order.
+			final String[] browsers = {
+				"epiphany", "firefox", "mozilla", "konqueror", "galeon", "netscape", "opera",
+				"links", "lynx" };
 
-		if (p.exitValue() < 0) {
-			throw new IOException(
-					"Unable to launch " + browserCommand + ".  Please check it, or launch " + url + " in your browser.");
+			// Build a command string which looks like
+			// "browser1 "url" || browser2 "url" || ..."
+			StringBuffer cmd = new StringBuffer();
+
+			for (int i = 0; i < browsers.length; i++) {
+				cmd.append((i == 0 ? "" : " || ") + browsers[i] + " \"" + url + "\" ");
+			}
+
+			rt.exec(new String[] { "sh", "-c", cmd.toString() });
 		}
 	}
 }
