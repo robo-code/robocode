c17952ef0f5ef57f839598e045d377a1d36bb5a5
Pavel Savara <pavel.savara@gmail.com>
2010-02-01 17:24:48 +0000

    headers

--- plugins/dotnet/robocode.dotnet.nhost/src/mono/reflection/CILReader.cs
+++ plugins/dotnet/robocode.dotnet.nhost/src/mono/reflection/CILReader.cs
@@ -1,4 +1,14 @@
-﻿//
+﻿#region Copyright (c) 2001, 2010 Mathew A. Nelson and Robocode contributors
+
+// Copyright (c) 2001, 2008 Mathew A. Nelson and Robocode contributors
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Common Public License v1.0
+// which accompanies this distribution, and is available at
+// http://robocode.sourceforge.net/license/cpl-v10.html
+
+#endregion
+#region (C) 2009 Novell, Inc. (http://www.novell.com)
+//
 // MethodBaseRocks.cs
 //
 // Author:
@@ -25,6 +35,7 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
+#endregion
 
 using System;
 using System.Collections.Generic;
--- plugins/dotnet/robocode.dotnet.robotscs/src/robocode/BadNamespaceCs.cs
+++ plugins/dotnet/robocode.dotnet.robotscs/src/robocode/BadNamespaceCs.cs
@@ -1,14 +1,13 @@
-/*******************************************************************************
- * Copyright (c) 2001, 2008 Mathew A. Nelson and Robocode contributors
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Common Public License v1.0
- * which accompanies this distribution, and is available at
- * http://robocode.sourceforge.net/license/cpl-v10.html
- *
- * Contributors:
- *     Pavel Savara
- *     - Initial implementation
- *******************************************************************************/
+#region Copyright (c) 2001, 2010 Mathew A. Nelson and Robocode contributors
+
+// Copyright (c) 2001, 2008 Mathew A. Nelson and Robocode contributors
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Common Public License v1.0
+// which accompanies this distribution, and is available at
+// http://robocode.sourceforge.net/license/cpl-v10.html
+
+#endregion
+
 namespace robocode
 {
     public class BadNamespaceCs : Robot
