5f99f3d8d1150227b8bb1448c6e544f74556e927
Pavel Savara <pavel.savara@gmail.com>
2010-02-15 23:52:44 +0000

    oh, too bad, .NET was not running at all.

--- plugins/dotnet/robocode.dotnet.content/src/main/resources/robocode.bat
+++ plugins/dotnet/robocode.dotnet.content/src/main/resources/robocode.bat
@@ -1,1 +1,1 @@
-java -Xmx512M -Dsun.io.useCanonCaches=false -cp libs/robocode.jar;libs/jni4net.j-0.6.0.0.jar robocode.Robocode %*
+java -Xmx512M -Dsun.io.useCanonCaches=false -cp libs/robocode.jar;libs/jni4net.j-0.7.0.0.jar robocode.Robocode %*
