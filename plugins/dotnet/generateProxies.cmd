d4c4627fbfe103d3d54d3471c619519641f3e62d
Flemming N Larsen <flemming.n.larsen@gmail.com>
2010-05-28 20:57:26 +0000

    Preparing for Robocode version 1.7.2.0 (final) release.

--- plugins/dotnet/generateProxies.cmd
+++ plugins/dotnet/generateProxies.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set JAVA_HOME=C:\Program Files\Java\jdk1.5.0_20
+set JAVA_HOME=C:\Program Files\Java\jdk1.5.0_22
 
 if exist "%~dp0\tools\lib\jni4net.j-0.7.1.0.jar" goto gen
 call "%~dp0\tools\loadTools.cmd"
