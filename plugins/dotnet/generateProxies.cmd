1650f63fec96ef63b68a02156ad35df668767b44
Pavel Savara <pavel.savara@gmail.com>
2010-07-19 23:48:10 +0000

    - upgraded to jni4net 0.8 with CLR 4.0 - implemented F# robot in VS2010

--- plugins/dotnet/generateProxies.cmd
+++ plugins/dotnet/generateProxies.cmd
@@ -2,9 +2,9 @@
 
 set JAVA_HOME=C:\Program Files\Java\jdk1.5.0_22
 
-if exist "%~dp0\tools\lib\jni4net.j-0.7.1.0.jar" goto gen
+if exist "%~dp0\tools\lib\jni4net.j-0.8.0.0.jar" goto gen
 call "%~dp0\tools\loadTools.cmd"
 
 :gen
-"%~dp0\tools\lib\jni4net.proxygen-0.7.1.0.exe" tools\proxygen\robocode.proxygen.xml
+"%~dp0\tools\lib\jni4net.proxygen-0.8.0.0.exe" tools\proxygen\robocode.proxygen.xml
 
--- plugins/dotnet/robocode.dotnet.content/src/main/resources/robocode.bat
+++ plugins/dotnet/robocode.dotnet.content/src/main/resources/robocode.bat
@@ -1,1 +1,1 @@
-java -Xmx512M -Dsun.io.useCanonCaches=false -cp libs/robocode.jar;libs/jni4net.j-0.7.1.0.jar robocode.Robocode %*
+java -Xmx512M -cp libs/robocode.jar;libs/jni4net.j-0.8.0.0.jar robocode.Robocode %*
