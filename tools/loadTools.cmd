ea75897eb692102a39151dba46afc570f13be498
Flemming N Larsen <flemming.n.larsen@gmail.com>
2010-10-12 20:46:09 +0000

    Fixed loadTools.cmd and loadTools.sh, so it can handle file paths containing spaces. Problem reported on the Help forum.

--- tools/loadTools.cmd
+++ tools/loadTools.cmd
@@ -1,2 +1,2 @@
 @echo off
-java -cp %~dp0/loader Loader http://robocode.sourceforge.net/tools/libs/ %~dp0/lib/ ant.jar ant-contrib-0.6.jar ant-launcher.jar jacobe.jar junit.jar classworlds-1.1.jar maven-2.0.9-uber.jar
+java -cp "%~dp0/loader" Loader http://robocode.sourceforge.net/tools/libs/ "%~dp0/lib/" ant.jar ant-contrib-0.6.jar ant-launcher.jar jacobe.jar junit.jar classworlds-1.1.jar maven-2.0.9-uber.jar
