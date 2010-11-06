da24951796a9f9a2bde89c5f6376e696191106b4
Flemming N Larsen <flemming.n.larsen@gmail.com>
2010-11-06 23:04:08 +0000

    Fixed the Version class due to a broken test result with the VersionTest test unit.

--- robocode.core/src/main/java/net/sf/robocode/version/Version.java
+++ robocode.core/src/main/java/net/sf/robocode/version/Version.java
@@ -35,7 +35,8 @@ public class Version implements Comparable<Object> {
 		if (!version.matches("\\s*[0-9]+\\.[0-9]+(\\.[0-9]+)?(\\.[0-9]+)?(\\s?(([aA]lpha)|([bB]eta))(\\s?[0-9]+)?)?\\s*")) {
 			throw new IllegalArgumentException("The format of the version string is not a valid");
 		}
-		this.version = version.trim();
+		version = version.trim();
+		this.version = version;
 
 		// Split the version number into its integer numbers
 		final String[] numbers = version.split("\\.");
