eb88a3ad3ec3c3ddfbb0fadbc59a42744b3970a8
Flemming N Larsen <flemming.n.larsen@gmail.com>
2011-07-08 22:28:23 +0000

    Bug [3355603] - Rumble sh scripts for launching do not handle spaces in path.

--- robocode.content/src/main/resources/robocode.sh
+++ robocode.content/src/main/resources/robocode.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 pwd=`pwd`
-cd ${0%/*}
+cd "${0%/*}"
 java -Xmx512M -cp libs/robocode.jar robocode.Robocode $*
-cd $pwd
+cd "${pwd}"
--- robocode.content/src/main/resources/roborumble.sh
+++ robocode.content/src/main/resources/roborumble.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 pwd=`pwd`
-cd ${0%/*}
+cd "${0%/*}"
 java -Xmx512M -cp libs/robocode.jar:libs/roborumble.jar:libs/codesize-1.1.jar roborumble.RoboRumbleAtHome ./roborumble/roborumble.txt
-cd $pwd
+cd "${pwd}"
