b9836c56d39405a22b323a371e2d6fa7196c69b7
Flemming N Larsen <flemming.n.larsen@gmail.com>
2012-04-25 22:09:39 +0000

    Version 1.7.3.6 Alpha 2

--- mvnassembly.cmd
+++ mvnassembly.cmd
@@ -3,4 +3,4 @@ if exist "%~dp0\tools\lib\maven-2.2.1-uber.jar" goto mvn
 call "%~dp0\tools\loadTools.cmd"
 
 :mvn
-@call "%~dp0\tools\bin\mvn.bat" clean install ant:ant -DskipTests=true %*
+@call "%~dp0\tools\bin\mvn.bat" clean install ant:ant -DskipTests=false %*
