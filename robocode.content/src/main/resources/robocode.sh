3807a9cffb149cb468aa71d8956bb6a10f5ee101
Flemming N Larsen <flemming.n.larsen@gmail.com>
2012-07-14 22:02:30 +0000

    * Bug [3543475] - All Text Missing. Reverting enabling OpenGL for Linux per default (introduced with Bug fix [3520616]) * Bug [3544205] - Robocode crash on window resize (linux-opengl). Reverting enabling OpenGL per default for Linux.

--- robocode.content/src/main/resources/robocode.sh
+++ robocode.content/src/main/resources/robocode.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 pwd=`pwd`
 cd "${0%/*}"
-java -Xmx512M -Dsun.java2d.opengl=True -cp libs/robocode.jar robocode.Robocode $*
+java -Xmx512M -cp libs/robocode.jar robocode.Robocode $*
 cd "${pwd}"
