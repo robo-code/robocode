dfde3dd27616329954aae96bd98a5b8693f4973b
flemming-n-larsen <flemming.n.larsen@gmail.com>
2013-03-27 22:52:23 +0100

    Updated the ReadMe.txt for the .NET plug-in + fixed the .classpath files and updated the generateProxies.cmd

--- plugins/dotnet/generateProxies.cmd
+++ plugins/dotnet/generateProxies.cmd
@@ -1,7 +1,10 @@
 @echo off
 
-set JAVA_HOME=%JDK6%
-set PATH=%JAVA_HOME%\bin;%PATH%
+rem set JAVA_HOME=%JDK6_HOME%
+rem set PATH=%JAVA_HOME%\bin;%PATH%
+
+set NET_FRAMEWORK_HOME=C:\Windows\Microsoft.NET\Framework\v3.5
+set PATH=%NET_FRAMEWORK_HOME%;%PATH%
 
 if exist "%~dp0\tools\lib\jni4net.j-0.8.6.0.jar" goto gen
 call "%~dp0\tools\loadTools.cmd"
@@ -15,14 +18,14 @@ echo cat't find robocode.dotnet.nhost\target\robocode.dotnet.nhost.dll, please c
 goto end
 
 :gen1
-if exist "%~dp0\tools\lib\robocode.dotnet.api.dll" goto gen3
-if exist "%~dp0\robocode.dotnet.api\target\robocode.dotnet.api.dll" goto gen2
-echo cat't find \robocode.dotnet.api\target\robocode.dotnet.api.dll, please compile it
+if exist "%~dp0\tools\lib\robocode.dll" goto gen3
+if exist "%~dp0\robocode.dotnet.api\target\robocode.dll" goto gen2
+echo cat't find \robocode.dotnet.api\target\robocode.dll, please compile it
 goto end
 
 :gen2
 
-copy "%~dp0\robocode.dotnet.api\target\robocode.dotnet.api.dll" "%~dp0\tools\lib"
+copy "%~dp0\robocode.dotnet.api\target\robocode.dll" "%~dp0\tools\lib"
 
 :gen3
 "%~dp0\tools\lib\jni4net.proxygen-0.8.6.0.exe" tools\proxygen\robocode.proxygen.net.xml
