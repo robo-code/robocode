0e1515f123e9f2e42723b9c7d659f1ba9a3535e1
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2014-09-15 20:33:09 +0200

    Version 1.9.2.3: Bug-373 (Wrong robot size calculation in version 1.9.0 - 1.9.2) was only partly fixed with version 1.9.2.2.

--- plugins/dotnet/robocode.dotnet.samples/src/SampleCsSentry/BorderGuard.cs
+++ plugins/dotnet/robocode.dotnet.samples/src/SampleCsSentry/BorderGuard.cs
@@ -55,7 +55,7 @@ namespace SampleCsSentry
     /// <p>
     /// Version: 1.0
     /// <p>
-    /// Since 1.9.2.2
+    /// Since 1.9.2.3
     /// </summary>
     public class BorderGuard
         : AdvancedRobot, IBorderSentry
