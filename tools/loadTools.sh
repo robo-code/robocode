9dbc55cea3ee022e33a2b21e7000ba51efe71ece
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2014-11-10 22:24:15 +0100

    Deleted the /tools/loader/Loader.class, which should not be included in the source files. Instead, this is now automatically being compiled the first time it is used.

--- tools/loadTools.sh
+++ tools/loadTools.sh
@@ -14,4 +14,8 @@ if [ ! -d "$dp0/lib" ]; then
    mkdir "$dp0/lib"
 fi
 
+if [ ! -e "$dp0/loader/Loader.class" ]; then
+   javac "$dp0/loader/Loader.java"
+fi
+
 java -cp "$dp0/loader" Loader http://robocode.sourceforge.net/tools/libs/ "$dp0/lib/" ant.jar ant-contrib-0.6.jar ant-launcher.jar jacobe.jar junit.jar classworlds-1.1.jar maven-2.2.1-uber.jar
