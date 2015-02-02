d8b29cd669ae7caaaa89f8becaae9676c45c667a
flemming-n-larsen <flemming.n.larsen@gmail.com>
2015-02-02 22:36:03 +0100

    Removed user-specifik JAVA_HOME nad PATH settings from mvn.cmd and mvnassembly.cmd.

--- mvn.cmd
+++ mvn.cmd
@@ -1,7 +1,7 @@
 @echo off
 
-SET JAVA_HOME=C:\Robocode\thoma\Downloads\jdk1.6.0_12
-SET PATH=%PATH%;%JAVA_HOME%\bin 
+rem SET JAVA_HOME=C:\Robocode\thoma\Downloads\jdk1.6.0_12
+rem SET PATH=%PATH%;%JAVA_HOME%\bin 
 
 if not exist "%~dp0\tools\lib\maven-*-uber.jar" (
    call "%~dp0\tools\loadTools.cmd"
--- mvnassembly.cmd
+++ mvnassembly.cmd
@@ -1,7 +1,7 @@
 @echo off
 
-SET JAVA_HOME=C:\Robocode\thoma\Downloads\jdk1.6.0_12
-SET PATH=%PATH%;%JAVA_HOME%\bin 
+rem SET JAVA_HOME=C:\Robocode\thoma\Downloads\jdk1.6.0_12
+rem SET PATH=%PATH%;%JAVA_HOME%\bin 
 
 mvn clean install ant:ant -DskipTests=true %*
 rem mvn eclipse:eclipse
