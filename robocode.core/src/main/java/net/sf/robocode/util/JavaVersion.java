9b43b19a64c5ef53b1f5d02a5ad152cf0c27217f
Flemming N. Larsen <fnl@users.sourceforge.net>
2018-11-06 22:03:36 +0100

    Added missing header files with license and copyright.

--- robocode.core/src/main/java/net/sf/robocode/util/JavaVersion.java
+++ robocode.core/src/main/java/net/sf/robocode/util/JavaVersion.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2001-2018 Mathew A. Nelson and Robocode contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://robocode.sourceforge.io/license/epl-v10.html
+ */
 package net.sf.robocode.util;
 
 public final class JavaVersion {
