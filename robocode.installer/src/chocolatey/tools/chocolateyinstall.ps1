42196e69e0e63bfe424c59353627474a23f8a2c8
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2022-01-17 23:02:39 +0100

    Revert "Fixed the nuget package for Chocolatey to the user can choose the installation directory."
    This reverts commit 8771fa7f7d7f9c94b224050c8c18c6b3dc9cbaeb.

--- robocode.installer/src/chocolatey/tools/chocolateyinstall.ps1
+++ robocode.installer/src/chocolatey/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 # Get-ChocolateyUnzip -FileFullPath  "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\robocode-*-setup.jar" -Destination "C:\robocode"
-Start-ChocolateyProcessAsAdmin -Statements "java -jar $(Split-Path -Parent $MyInvocation.MyCommand.Definition)\robocode-$env:ChocolateyPackageVersion-setup.jar"
+Start-ChocolateyProcessAsAdmin -Statements "java -jar $(Split-Path -Parent $MyInvocation.MyCommand.Definition)\robocode-$env:ChocolateyPackageVersion-setup.jar C:\robocode\ silent"
