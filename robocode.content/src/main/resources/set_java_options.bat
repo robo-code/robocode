092e493dc80a79d2f35c108fc05cba9d28d0e774
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2025-05-13 23:02:48 +0200

    Updated copyright year

--- robocode.content/src/main/resources/set_java_options.bat
+++ robocode.content/src/main/resources/set_java_options.bat
@@ -1,5 +1,5 @@
 @rem
-@rem Copyright (c) 2001-2023 Mathew A. Nelson and Robocode contributors
+@rem Copyright (c) 2001-2025 Mathew A. Nelson and Robocode contributors
 @rem All rights reserved. This program and the accompanying materials
 @rem are made available under the terms of the Eclipse Public License v1.0
 @rem which accompanies this distribution, and is available at
